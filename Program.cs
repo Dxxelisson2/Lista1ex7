960b91cf13758a705e6586520edf85f58206a248
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 20:57:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhasn;
+            double km;
+            Console.WriteLine("conversor de milhas nauticas para km");
+
+            Console.WriteLine("Digite as milhas nauticas");
+            milhasn = double.Parse(Console.ReadLine());
+            km = milhasn * 1.852;
+            Console.WriteLine("valor em km = "+ km);
+        }
+    }
+}
